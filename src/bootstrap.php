1e5cdb8c6bc9484852bf58b70ba64e88a5347697
Justin Kopepasah <justin@kopepasah.com>
2020-06-21 01:51:15 -0500

    Hello World. 👋🏻

---
+++ src/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bootstrap the plugin.
+ *
+ * @package S4B
+ */
+
+namespace S4B;
+
+// Define a couple constants, to make things easier. 😎
+define( 'S4B_DIR', \plugin_dir_path( __FILE__ ) );
+define( 'S4B_URL', \plugin_dir_url( __FILE__ ) );
+
+// Let's get this party started. 🥳
---
+++ stripe-for-business.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Stripe for Business
+ *
+ * @package   S4B
+ * @author    Justin Kopepasah
+ * @license   MPL-2.0
+ *
+ * Plugin Name:       Stripe for Business
+ * Plugin URI:        https://juko.com/wp/plugins/stripe-for-business
+ * Description:       Simple. Fast. Secure. Run your store with Stripe for Business, the only plugin with full Stripe integration to manage your business using WordPress.
+ * Version:           0.0.1
+ * Requires at least: 5.4
+ * Requires PHP:      7.2
+ * Author:            Justin Kopepasah
+ * Author URI:        https://kopepasah.com
+ * Text Domain:       stripe-for-business
+ * License:           MPL-2.0
+ * License URI:       https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+require_once \plugin_dir_path( __FILE__ ) . 'src/bootstrap.php';
